c7cf3d3ee8d1dfdda01cb5ab7954a7f0da60a774
Kenny <kenny_oliver@icloud.com>
2021-02-24 11:00:45 +0000

    [+] Add menu() [+] Prevent user from entering more than 1 character for char_to_unicode() [+] Removed strings containing ASCCI, replaced them with Unicode [+] Made UI cleaner [+] Add comments

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-def intoASCII():
-  reply = "y"
-  input_char = input("Enter a character to be converted to ASCII: ")
+def char_to_unicode():
+  print("char --> Unicode")
+  input_char = input("Enter character: ")
+  while len(input_char) != 1:
+    input_char = input("Must be 1 character: ")
   print(f"\"{input_char}\" in Unicode is {ord(input_char)}")
 #====================
-def intoEnglish():
-  reply = "y"
-  input_code = int(input("Enter ASCII to be converted into characters: "))
-  print(f"\"{input_code}\" in Unicode is {chr(input_code)}")
-  reply = input("Do you want to convert more ASCII?\n--> ").lower()
-  if reply == "y":
-    intoEnglish()
+def unicode_to_char():
+  print("Unicode --> char")
+  input_code = int(input("Enter Unicode: "))
+  print(f"{input_code} in Unicode is \"{chr(input_code)}\"")
+#====================
+def menu():
+  print("Choose method:\n\t[1] char --> Unicode\n\t[2] Unicode --> char")
+  option = input("\t--> ")
+  while int(option) not in [1,2]:
+    option = input("\n\t--> ")
+  option = int(option) #cast to integer once valid
+  
+  if option == 1:
+    char_to_unicode()
+    menu() #recursive
+  elif option == 2:
+    unicode_to_char()
+    menu() #recursive
 #====================
 # MAIN PROGRAM
-intoASCII()
-intoEnglish()
+menu()
